2332d6c8cc35b1008988090ea6363c69ac2b407b
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-01-31 22:04:51 +0530

    feat working on cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,18 +1,71 @@
 import 'package:flutter/material.dart';
 
-class Cart with ChangeNotifier {
+class CartItem {
   final String id;
-  final String name;
-  final String description;
-  final String image;
-  final double price;
+  final String title;
   final int quantity;
+  final double price;
 
-  Cart(
-      {required this.id,
-      required this.name,
-      required this.description,
-      required this.image,
-      required this.price,
-      required this.quantity});
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      // change quantity...
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
 }
